7354a384625a4b6d882395de9b65fe22ce2b15d3
Ernesto López <jesus.lopez252663@potros.itson.edu.mx>
2025-04-01 20:21:06 -0700

    Creacion de Entidades
    Se crearon las entidades para el módulo individual de Productos

---
+++ Restaurante/src/main/java/Entidades/ProductoIngrediente.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ *
+ * @author ErnestoLpz_252663
+ */
+@Entity
+@Table(name = "producto_ingrediente")
+public class ProductoIngrediente implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    @ManyToOne
+    @JoinColumn(name = "ingrediente_id", nullable = false)
+    private Ingrediente ingrediente;
+
+    @Column(nullable = false)
+    private double cantidadRequerida;
+
+    public ProductoIngrediente() {
+    }
+
+    public ProductoIngrediente(Long id, Producto producto, Ingrediente ingrediente, double cantidadRequerida) {
+        this.id = id;
+        this.producto = producto;
+        this.ingrediente = ingrediente;
+        this.cantidadRequerida = cantidadRequerida;
+    }
+
+    public ProductoIngrediente(Producto producto, Ingrediente ingrediente, double cantidadRequerida) {
+        this.producto = producto;
+        this.ingrediente = ingrediente;
+        this.cantidadRequerida = cantidadRequerida;
+    }
+    
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Ingrediente getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Ingrediente ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public double getCantidadRequerida() {
+        return cantidadRequerida;
+    }
+
+    public void setCantidadRequerida(double cantidadRequerida) {
+        this.cantidadRequerida = cantidadRequerida;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoIngrediente{" + "id=" + id + ", producto=" + producto + ", ingrediente=" + ingrediente + ", cantidadRequerida=" + cantidadRequerida + '}';
+    }
+}
