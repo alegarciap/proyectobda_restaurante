b5e1ae589dce3b1737a42cf8df5af4de5ce41dbc
alegarciap <alegarpre@gmail.com>
2025-04-04 16:42:33 -0600

    Prueba para que salgan los paquetes y creación de NegocioException

---
+++ restauranteNegocio/src/main/java/BO/IngredienteBO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BO;
+
+import interfaces.IIngredienteBO;
+
+/**
+ *
+ * @author alega
+ */
+public class IngredienteBO implements IIngredienteBO {
+    
+}
